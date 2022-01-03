c6f83c8f2d71782ee749a08e547a68336ce8895e
Shubham Arya <60827845+TheShubhamArya@users.noreply.github.com>
2022-01-03 16:23:33 -0600

    Create ThreeSum.swift

---
+++ ThreeSum.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func threeSum(_ nums: [Int]) -> [[Int]] {
+        var output = [[Int]]()
+        let nums = nums.sorted()
+        for (i,num) in nums.enumerated(){
+            var start = i+1
+            var last = nums.count - 1
+            while start < last {
+                let sum = num + nums[start] + nums[last]
+                if sum == 0 {
+                    let array = [num, nums[start],nums[last]]
+                    if !output.contains(array){
+                        output.append(array)
+                    }
+                    last -= 1
+                    start += 1
+                } else if sum > 0 {
+                    last -= 1
+                } else if sum < 0 {
+                    start += 1
+                }
+            }
+        }
+        
+        return output
+    }
+}
