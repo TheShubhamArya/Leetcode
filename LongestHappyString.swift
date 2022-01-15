5fa82ddd44cf2101e9da8ece774c2fcd606e07bb
Shubham Arya <60827845+TheShubhamArya@users.noreply.github.com>
2022-01-15 16:24:42 -0600

    Update LongestHappyString.swift
    Add comments as explanation

--- LongestHappyString.swift
+++ LongestHappyString.swift
@@ -11,9 +11,10 @@ class Solution {
         while a != 0 || b != 0 || c != 0 {
             
             if useHighest {
+                // If letter is highest and was not used last
                 if a >= b && a >= c && lastUsed != "a"{
                     (output, a, lastUsed) = createOutputString(with: "a", output, a, useHighest)
-                } else if b >= a && b >= c && lastUsed != "b"{
+                } else if b >= a && b >= c && lastUsed != "b"{ 
                     (output, b, lastUsed) = createOutputString(with: "b", output, b, useHighest)
                 } else if c >= a && c >= b && lastUsed != "c"{
                     (output, c, lastUsed) = createOutputString(with: "c", output, c, useHighest)
@@ -22,14 +23,16 @@ class Solution {
                 }
                 
             } else {
-                
-                if ((a >= b && a <= c) || (a >= c && a <= b)) && a > 0 && lastUsed != "a"{
+                // If letter is second highest AND greater than 0 AND was not used last
+                if ((a >= b && a <= c) || (a >= c && a <= b)) && a > 0 && lastUsed != "a"{ 
                     (output, a, lastUsed) = createOutputString(with: "a", output, a, useHighest)
                 } else if ((b >= c && b <= a) || (b >= a && b <= c)) && b > 0 && lastUsed != "b"{
                     (output, b, lastUsed) = createOutputString(with: "b", output, b, useHighest)
                 } else if ((c >= a && c <= b) || (c >= b && c <= a)) && c > 0 && lastUsed != "c"{
                     (output, c, lastUsed) = createOutputString(with: "c", output, c, useHighest)
-                } else if a != 0 && b == 0 && c == 0 && lastUsed != "a" {
+                } 
+                // When one letter is greater than 0 while others are zero and that letter is not last used
+                else if a != 0 && b == 0 && c == 0 && lastUsed != "a" {
                     (output, a, lastUsed) = createOutputString(with: "a", output, a, useHighest)
                      break
                 } else if b != 0 && a == 0 && c == 0 && lastUsed != "b" {
