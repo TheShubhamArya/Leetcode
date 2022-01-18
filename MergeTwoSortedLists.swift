bc0a4fc0bce1d123f6e1391a4490c0e848b52329
Shubham Arya <60827845+TheShubhamArya@users.noreply.github.com>
2022-01-18 17:54:55 -0600

    Create MergeTwoSortedLists.swift

---
+++ MergeTwoSortedLists.swift
@@ -0,0 +1,28 @@
+// 21. Merge Two Sorted Lists
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+       guard let l1 = list1 else {
+           return list2
+       }
+        guard let l2 = list2 else {
+            return list1
+        }
+        if l1.val <= l2.val {
+            l1.next = mergeTwoLists(l1.next, l2)
+            return l1
+        } else {
+            l2.next = mergeTwoLists(l1,l2.next)
+            return l2
+        }
+    }
+}
