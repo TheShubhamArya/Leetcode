0c784da3513c67da638ed79e440990c7666be9f5
Shubham Arya <60827845+TheShubhamArya@users.noreply.github.com>
2022-02-20 21:37:12 -0600

    Create JumpGame.swift

---
+++ Medium/JumpGame.swift
@@ -0,0 +1,41 @@
+// 55. Jump Game
+
+class Solution {
+    func canJump(_ nums: [Int]) -> Bool {
+        var current = 0
+        
+        if (nums.first! == 0 && nums.count == 1) {
+            return true
+        }
+        
+        if nums.isEmpty || nums.first! == 0 {
+            return false
+        }
+        
+        for num in nums {
+            let currentNum = nums[current]
+            
+            if current + currentNum >= (nums.count - 1) || current == (nums.count - 1) {
+                return true
+            } else if currentNum == 0 {
+                return false
+            }
+            
+            let possibleSteps = Array(nums[current+1...(current+currentNum)])
+            var maximum = nums[current + 1]
+            var nextStep = 0
+            
+            for (i,step) in possibleSteps.enumerated() {
+                let distance = step + current + i + 1
+                if distance >= maximum {
+                    maximum = distance
+                    nextStep = i
+                }
+            }
+            current += (nextStep + 1)
+            
+        }
+        
+        return false
+    }
+}
