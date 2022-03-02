26cf78a9a612eebbd50871410023487dd5d1bc5c
Shubham Arya <60827845+TheShubhamArya@users.noreply.github.com>
2022-03-02 01:07:27 -0600

    Create PreorderTraversal.swift

---
+++ Tree/PreorderTraversal.swift
@@ -0,0 +1,31 @@
+// 144. Binary Tree Preorder Traversal
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func preorderTraversal(_ root: TreeNode?) -> [Int] {
+        var output = [Int]()
+        traversal(root,&output)
+        return output
+    }
+    func traversal(_ root: TreeNode?,_ output: inout [Int]) {
+        if root == nil {
+            return 
+        }
+        output.append(root!.val)
+        traversal(root?.left, &output)
+        traversal(root?.right, &output)
+    }
+}
