757de5d500ea0f359966215891c84ab7c43ea6ec
Shubham Arya <60827845+TheShubhamArya@users.noreply.github.com>
2022-03-04 14:20:24 -0600

    Create InvertTree.swift

---
+++ Tree/InvertTree.swift
@@ -0,0 +1,29 @@
+// 226. Invert Binary Tree
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        if root == nil {
+            return root
+        }
+        invertTree(root?.left)
+        invertTree(root?.right)
+        var left = root?.left
+        root?.left = root?.right
+        root?.right = left
+        return root
+    }
+}
